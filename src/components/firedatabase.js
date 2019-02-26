3dbc00d67bc4fd54d5b5941915784fdc2ab18f0e
asnaniyaz <35967005+asnaniyaz@users.noreply.github.com>
2019-02-26 20:47:21 +0400

    Update firedatabase.js

--- src/components/firedatabase.js
+++ src/components/firedatabase.js
@@ -1,12 +1,12 @@
 import Firebase from 'firebase'
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyANq0tvszwP8qHeJCHqUOPDobMmwcweCnA",
-    authDomain: "userdetails-5d417.firebaseapp.com",
-    databaseURL: "https://userdetails-5d417.firebaseio.com",
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
     projectId: "userdetails-5d417",
-    storageBucket: "userdetails-5d417.appspot.com",
-    messagingSenderId: "914963999803"
+    storageBucket: "#",
+    messagingSenderId: "#
   };
   let app = Firebase.initializeApp(config);
   let db = app.database();
